5fffb1f1be2839be80d11379bf48e037ebd6d191
Cristian Ahumada <cdahumada35@ucatolica.edu.co>
2026-02-25 17:46:58 -0500

    correcion games v2

--- src/games/games.py
+++ src/games/games.py
@@ -3,36 +3,31 @@ class Games:
         jugador1 = jugador1.lower()
         jugador2 = jugador2.lower()
 
+        opciones = ["piedra", "papel", "tijera"]
+
+        if jugador1 not in opciones or jugador2 not in opciones:
+            return "invalid"
+
         if jugador1 == jugador2:
             return "empate"
 
-        gana_a = {
-            "piedra": "tijera",
-            "tijera": "papel",
-            "papel": "piedra"
-        }
-
-        if gana_a[jugador1] == jugador2:
+        if (
+            (jugador1 == "piedra" and jugador2 == "tijera") or
+            (jugador1 == "papel" and jugador2 == "piedra") or
+            (jugador1 == "tijera" and jugador2 == "papel")
+        ):
             return "jugador1"
+
         return "jugador2"
     
-    def adivinar_numero_pista(self, numero_secreto, intento):
-        if intento == numero_secreto:
-            return "correcto"
-        elif intento > numero_secreto:
-            return "muy alto"
-        else:
-            return "muy bajo"
-    
     def ta_te_ti_ganador(self, tablero):
-        
-        for i in range(3):
-            if tablero[i][0] == tablero[i][1] == tablero[i][2] != " ":
-                return tablero[i][0]
-            
-        for i in range(3):
-            if tablero[0][i] == tablero[1][i] == tablero[2][i] != " ":
-                return tablero[0][i]
+        for fila in tablero:
+            if fila[0] == fila[1] == fila[2] != " ":
+                return fila[0]
+
+        for col in range(3):
+            if tablero[0][col] == tablero[1][col] == tablero[2][col] != " ":
+                return tablero[0][col]
 
         if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
             return tablero[0][0]
