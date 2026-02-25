5860f54b12718c06719a16dce06f63375d7ddfca
Cristian Ahumada <cdahumada35@ucatolica.edu.co>
2026-02-25 18:26:30 -0500

    correcion magic

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -5,7 +5,9 @@ class Magic:
     """
     
     def fibonacci(self, n):
-        if n <= 0:
+        if n < 0:
+            return None
+        if n == 0:
             return 0
         if n == 1:
             return 1
