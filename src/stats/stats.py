efb92aa993b898c42a75bddbbe09e809e35e85c2
Cristian Ahumada <cdahumada35@ucatolica.edu.co>
2026-02-25 19:13:49 -0500

    correccion stats

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -1,47 +1,50 @@
 class Stats:
     def promedio(self, numeros):
-        suma = 0
-        for n in numeros:
-            suma += n
-        return suma / len(numeros)
+        if not numeros:
+            return 0
+        return sum(numeros) / len(numeros)
     
     def mediana(self, numeros):
+        if not numeros:
+            return 0
+
         ordenados = sorted(numeros)
         n = len(ordenados)
         medio = n // 2
+
         if n % 2 == 0:
             return (ordenados[medio - 1] + ordenados[medio]) / 2
         return float(ordenados[medio])
     
     def moda(self, numeros):
+        if not numeros:
+            return None
+
         frecuencia = {}
         for n in numeros:
-            if n in frecuencia:
-                frecuencia[n] += 1
-            else:
-                frecuencia[n] = 1
-        moda = numeros[0]
-        for n in frecuencia:
-            if frecuencia[n] > frecuencia[moda]:
-                moda = n
-        return moda
+            frecuencia[n] = frecuencia.get(n, 0) + 1
+
+        max_frecuencia = max(frecuencia.values())
+
+        for n in numeros:
+            if frecuencia[n] == max_frecuencia:
+                return n
     
     def desviacion_estandar(self, numeros):
         return self.varianza(numeros) ** 0.5
     
     def varianza(self, numeros):
+        if not numeros:
+            return 0
+
         media = self.promedio(numeros)
         suma = 0
         for n in numeros:
             suma += (n - media) ** 2
+
         return suma / len(numeros)
     
     def rango(self, numeros):
-        minimo = numeros[0]
-        maximo = numeros[0]
-        for n in numeros:
-            if n < minimo:
-                minimo = n
-            if n > maximo:
-                maximo = n
-        return maximo - minimo
+        if not numeros:
+            return 0
+        return max(numeros) - min(numeros)
